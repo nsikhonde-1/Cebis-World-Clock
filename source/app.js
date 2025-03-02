e6a4d1bd694b1ed839aea0f26f810831bd2ece22
nsikhonde-1 <nsikhonde1@gmail.com>
2025-03-02 12:00:35 +0200

    update cities

--- source/app.js
+++ source/app.js
@@ -2,11 +2,32 @@ function updateWatch() {
     let cityName = document.querySelector("#cairo");
     let cityDate = cityName.querySelector(".date");
     let cityTime = cityName.querySelector(".time");
-    let cityTimeWatch = moment().tz("African/Cairo");
+    let cityTimeWatch = moment().tz("Africa/Cairo");
 
     cityDate.innerHTML = cityTimeWatch.format("dddd, Do MMMM YYYY");
 
     cityTime.innerHTML = cityTimeWatch.format("h:mm:ss [<small>]A[</small>]");
+
+
+    let cityNameCT = document.querySelector("#capetown");
+    let cityDateCT = cityNameCT.querySelector(".date");
+    let cityTimeCT = cityNameCT.querySelector(".time");
+    let cityTimeWatchCT = moment().tz("Africa/Johannesburg");
+
+    cityDateCT.innerHTML = cityTimeWatchCT.format("dddd, Do MMMM YYYY");
+
+    cityTimeCT.innerHTML = cityTimeWatchCT.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+
+    let cityNameManila = document.querySelector("#manila");
+    let cityDateManila = cityNameManila.querySelector(".date");
+    let cityTimeManila = cityNameManila.querySelector(".time");
+    let cityTimeWatchManila = moment().tz("Asia/Manila");
+
+    cityDateManila.innerHTML = cityTimeWatchManila.format("dddd, Do MMMM YYYY");
+
+    cityTimeManila.innerHTML = cityTimeWatchManila.format("h:mm:ss [<small>]A[</small>]");
 }
 
 
@@ -22,13 +43,13 @@ function updateCities(event) {
                <h2>${cityNameEle}</h2>
              <div class="date">${cityWatch.format("dddd, Do MMMM YYYY")}</div>
              </div>
-            <div class="time">${cityWatch.format("h:mm:ss")}<small>${cityWatch.format("A")}</small></div>
+            <div class="time">${cityWatch.format("h:mm:ss ")}<small>${cityWatch.format("A")}</small></div>
        </div>
       </div>`;
 
 }
 updateWatch();
-setInterval(updateWatch, 1000);  
+setInterval(updateWatch, 1000);
 
 let citySelect = document.querySelector("#city");
 citySelect.addEventListener("change", updateCities)
