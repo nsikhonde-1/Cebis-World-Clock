20110700d34c23d376c2b905fb41bc207d611fa9
nsikhonde-1 <nsikhonde1@gmail.com>
2025-03-02 11:34:55 +0200

    update tz in js

--- source/app.js
+++ source/app.js
@@ -9,5 +9,26 @@ function updateWatch() {
     cityTime.innerHTML = cityTimeWatch.format("h:mm:ss [<small>]A[</small>]");
 }
 
+
+
+function updateCities(event) {
+    let cityTimezone = event.target.value;
+    let cityNameEle = cityTimezone.replace("_", " ").split("/")[1];
+    let cityWatch = moment().tz(cityTimezone);
+    let cityElement = document.querySelector("#cities");
+    cityElement.innerHTML =`
+       <div class="city">
+             <div>
+               <h2>${cityNameEle}</h2>
+             <div class="date">${cityWatch.format("dddd, Do MMMM YYYY")}</div>
+             </div>
+            <div class="time">${cityWatch.format("h:mm:ss")}<small>${cityWatch.format("A")}</small></div>
+       </div>
+      </div>`;
+
+}
 updateWatch();
-setInterval(updateWatch, 1000)
+setInterval(updateWatch, 1000);  
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCities)
